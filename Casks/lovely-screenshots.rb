aa9fbb3312ed26628189d2eb6709b3c74792e5e2
Jakub Kusmider <TWOJ_PRYWATNY_EMAIL@example.com>
2026-07-14 07:58:32 +0200

    0.4.3 — onboarding + editor + comp codes

--- Casks/lovely-screenshots.rb
+++ Casks/lovely-screenshots.rb
@@ -1,6 +1,6 @@
 cask "lovely-screenshots" do
-  version "0.4.2"
-  sha256 "7683c0fee541659fb257369b4022ce9a80b008df1dd040ce065ca36e741d849e"
+  version "0.4.3"
+  sha256 "7bbda609db4c7d83eb08ec6b14422ee3e78eb66aa1fe7c4471078aa669510a70"
 
   url "https://github.com/kusmiderdev/lovely-screenshots-releases/releases/download/v#{version}/Lovely-Screenshots-#{version}.zip"
   name "Lovely Screenshots"
